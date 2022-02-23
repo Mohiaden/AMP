f96b569b6d7fb3df63d574d1333bb0451ef359f8
Mohiaden Mohamoud Mohamed <muxudiinkeey@outlook.com>
2022-02-23 21:56:49 +0100

    ok

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -2,8 +2,8 @@ import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core
 
 @Component({
   selector: 'pm-star',
-  templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  templateUrl: '../star/star.component.html',
+  styleUrls: ['../star/star.component.css']
 })
 export class StarComponent implements OnChanges {
   @Input() rating = 0;
